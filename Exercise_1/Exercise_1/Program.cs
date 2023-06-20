dd4c0f439aeeee892ed14739bcb002f2dc6b59d9
Leonardo Gama <leogama09@gmail.com>
2023-06-20 11:32:14 -0300

    Exercise 1 done

---
+++ Exercise_1/Exercise_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_1 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine("{0}, cujo preço é ${1:F2}", produto1, preco1);
+            Console.WriteLine("{0}, cujo preço é ${1:F2}", produto2, preco2);
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+            Console.WriteLine("Medida com 8 casas decimais: {0}", medida);
+            Console.WriteLine("Arredondado (três casas decimais): {0:F3}", medida);
+            Console.WriteLine("Separador decimal invariant culture:" + (medida.ToString("F3", CultureInfo.InvariantCulture)));
+        }
+    }
+}
