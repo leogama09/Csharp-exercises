746692a69b8b4a01c8a8d2f6afc8eea555222344
Leonardo Gama <leogama09@gmail.com>
2023-06-21 11:45:25 -0300

    Exercise 3 done

---
+++ Exercise_3/Exercise_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            int A, B, soma;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            soma = A + B;
+
+            Console.WriteLine("SOMA = " + soma);
+        }
+    }
+}
