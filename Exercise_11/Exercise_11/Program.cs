b40940a58069b51e3571182ea00764e30c6ddc37
Leonardo Gama <leogama09@gmail.com>
2023-06-22 11:57:24 -0300

    Exercise 11 done

---
+++ Exercise_11/Exercise_11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_11
+{
+    internal class Program {
+        static void Main(string[] args) {
+            string[] values = Console.ReadLine().Split(' ');
+            int A = int.Parse(values[0]);
+            int B = int.Parse(values[1]);
+
+            if (A % B == 0 || B % A == 0) {
+                Console.WriteLine("Are multiples");
+            }
+            else {
+                Console.WriteLine("Are not multiples");
+            }
+        }
+    }
+}
