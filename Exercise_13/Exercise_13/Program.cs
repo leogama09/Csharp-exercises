10e9751e1c4010009d6acf9c449a508b4f9c0218
Leonardo Gama <leogama09@gmail.com>
2023-06-22 12:26:59 -0300

    Exercise 14 done

--- Exercise_13/Exercise_13/Program.cs
+++ Exercise_13/Exercise_13/Program.cs
@@ -5,25 +5,25 @@ namespace Exercise_13 {
     class Program {
         static void Main(string[] args) {
 
-            string[] valores = Console.ReadLine().Split(' ');
-            int codigo = int.Parse(valores[0]);
-            int quantidade = int.Parse(valores[1]);
+            string[] values = Console.ReadLine().Split(' ');
+            int code = int.Parse(values[0]);
+            int quantity = int.Parse(values[1]);
 
             double total;
-            if (codigo == 1) {
-                total = quantidade * 4.0;
+            if (code == 1) {
+                total = quantity * 4.0;
             }
-            else if (codigo == 2) {
-                total = quantidade * 4.5;
+            else if (code == 2) {
+                total = quantity * 4.5;
             }
-            else if (codigo == 3) {
-                total = quantidade * 5.0;
+            else if (code == 3) {
+                total = quantity * 5.0;
             }
-            else if (codigo == 4) {
-                total = quantidade * 2.0;
+            else if (code == 4) {
+                total = quantity * 2.0;
             }
             else {
-                total = quantidade * 1.5;
+                total = quantity * 1.5;
             }
 
             Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
---
+++ Exercise_14/Exercise_14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_14 {
+    class Program {
+        static void Main(string[] args) {
+
+            double number = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (number < 0.0 || number > 100.0) {
+                Console.WriteLine("Out of range");
+            }
+            else if (number <= 25.0) {
+                Console.WriteLine("Range [0,25]");
+            }
+            else if (number <= 50.0) {
+                Console.WriteLine("Range (25,50]");
+            }
+            else if (number <= 75.0) {
+                Console.WriteLine("Range (50,75]");
+            }
+            else {
+                Console.WriteLine("Range (75,100]");
+            }
+        }
+    }
+}
