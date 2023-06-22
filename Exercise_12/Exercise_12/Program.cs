dc2291ab89f790e3183897ea7d7a57399cfd2bb4
Leonardo Gama <leogama09@gmail.com>
2023-06-22 12:09:02 -0300

    Exercise 12 done

---
+++ Exercise_12/Exercise_12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_12 {
+    class Program {
+        static void Main(string[] args) {
+
+            string[] values = Console.ReadLine().Split(' ');
+            int initialHour = int.Parse(values[0]);
+            int finalHour = int.Parse(values[1]);
+
+            int duration;
+            if (initialHour < finalHour) {
+                duration = finalHour - initialHour;
+            }
+            else {
+                duration = 24 - initialHour + finalHour;
+            }
+
+            Console.WriteLine("THE GAME LASTED " + duration + " HOURS");
+        }
+    }
+}
