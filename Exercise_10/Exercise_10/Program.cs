1542cb964cce8e1af06e0cca8fa94c5fe38912e2
Leonardo Gama <leogama09@gmail.com>
2023-06-22 11:52:08 -0300

    Exercise 10 done

---
+++ Exercise_10/Exercise_10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise_10
+{
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Is this number odd or even?");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0) {
+                Console.WriteLine("Even!");
+            } else {
+                Console.WriteLine("Odd!");
+            }
+        }
+    }
+}
