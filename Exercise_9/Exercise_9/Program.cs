b1125d0149d0b9f8a8d9537f24ebd48af7525f6b
Leonardo Gama <leogama09@gmail.com>
2023-06-22 11:43:11 -0300

    Exercise 9 done

---
+++ Exercise_9/Exercise_9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_9
+{
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Is this number negative?");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 0) {
+                Console.WriteLine("Negative!");
+            }
+            else
+            {
+                Console.WriteLine("Not Negative!");
+            }
+        }
+    }
+}
