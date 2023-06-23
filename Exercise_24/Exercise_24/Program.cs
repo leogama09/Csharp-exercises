6c92a554b765f0238c506bbae7fdfcc459624443
Leonardo Gama <leogama09@gmail.com>
2023-06-23 12:24:15 -0300

    Exercise 24 done

---
+++ Exercise_24/Exercise_24/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise_24 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int fat = 1;
+            for (int i = 1; i <= n; i++) {
+                fat = fat * i;
+            }
+
+            Console.WriteLine(fat);
+        }
+    }
+}
