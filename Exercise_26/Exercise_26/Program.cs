d8ca71c9a05a7be26ce0fb41c450dacc5ccfdffc
Leonardo Gama <leogama09@gmail.com>
2023-06-23 12:31:09 -0300

    Exercise 26 done

---
+++ Exercise_26/Exercise_26/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_26 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+
+                int first = i;
+                int second = i * i;
+                int third = i * i * i;
+                Console.WriteLine($"{first} {second} {third}");
+            }
+        }
+    }
+}
