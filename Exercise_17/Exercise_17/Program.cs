718c1210421fd0c9051ce2a86e1cf3a82c669271
Leonardo Gama <leogama09@gmail.com>
2023-06-23 11:39:23 -0300

    Exercise 17 done

---
+++ Exercise_17/Exercise_17/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise_17 {
+    class Program {
+        static void Main(string[] args) {
+
+            int password = int.Parse(Console.ReadLine());
+
+            while (password != 2002) {
+                Console.WriteLine("Invalid Password");
+                password = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Access Granted");
+        }
+    }
+}
