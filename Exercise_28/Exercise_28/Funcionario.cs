1090829110d9150f2a0602eea786ff78032d3fbb
Leonardo Gama <leogama09@gmail.com>
2023-06-26 11:20:24 -0300

    Exercise 28 done

---
+++ Exercise_28/Exercise_28/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_28 {
+    internal class Funcionario {
+        public string Nome;
+        public double Salario;
+    }
+}
