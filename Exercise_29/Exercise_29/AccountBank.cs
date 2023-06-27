aaaef3846992f755614e3bf7810207babf0cc0d6
Leonardo Gama <leogama09@gmail.com>
2023-06-27 11:53:23 -0300

    Exercise 29 done

---
+++ Exercise_29/Exercise_29/AccountBank.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_29 {
+    internal class AccountBank {
+
+        public int Number { get; private set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+
+        public AccountBank(int number, string holder) {
+            Number = number;
+            Holder = holder;
+        }
+
+        public AccountBank(int number, string holder, double balance) : this(number, holder) {
+            Balance = balance;
+        }
+
+        public void Deposit(double quantity) {
+           Balance += quantity;
+        }
+
+        public void Withdraw(double quantity) {
+            Balance -= quantity + 5.0;
+        }
+
+        public override string ToString() {
+            return "Account "
+                + Number
+                + ", Holder: "
+                + Holder
+                + ", Balance: $ "
+                + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercise_29/Exercise_29/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_29 {
+    class Program {
+        static void Main(string[] args) {
+
+            AccountBank account;
+
+            Console.Write("Enter the account number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Enter the account holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("There will be initial deposit (y/n)? ");
+            char ans = char.Parse(Console.ReadLine());
+            if (ans == 'y' || ans == 'Y') {
+                Console.Write("Enter the initial deposit amount: ");
+                double initialDeposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account = new AccountBank(number, holder, initialDeposit);
+            }
+            else {
+                account = new AccountBank(number, holder);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Account data:");
+            Console.WriteLine(account);
+
+            Console.WriteLine();
+            Console.Write("Enter a deposit amount: ");
+            double quantity = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Deposit(quantity);
+            Console.WriteLine("Updated account details:");
+            Console.WriteLine(account);
+
+            Console.WriteLine();
+            Console.Write("Enter a withdrawal amount: ");
+            quantity = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Withdraw(quantity);
+            Console.WriteLine("Updated account details:");
+            Console.WriteLine(account);
+        }
+    }
+}
